21ae8646a56cb84c5a4b3b9501f65c3c8205f26b
pupiwang <dreamjl@live.cn>
2018-12-19 14:34:34 +0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,154 @@
+import com.sap.conn.jco.*;
+import com.sap.conn.jco.ext.DestinationDataProvider;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+public class Main {
+    static String DESTINATION_NAME1 = "ABAP_AS_WITHOUT_POOL";
+    static String DESTINATION_NAME2 = "ABAP_AS_WITH_POOL";
+
+    static {
+        Properties connectProperties = new Properties();
+        connectProperties.setProperty(DestinationDataProvider.JCO_ASHOST, "sap752sp01.southeastasia.cloudapp.azure.com");
+        connectProperties.setProperty(DestinationDataProvider.JCO_SYSNR, "00");
+        connectProperties.setProperty(DestinationDataProvider.JCO_CLIENT, "001");
+        connectProperties.setProperty(DestinationDataProvider.JCO_USER, "TESTID01");
+        connectProperties.setProperty(DestinationDataProvider.JCO_PASSWD, "Sh0wM3TheLust");
+        connectProperties.setProperty(DestinationDataProvider.JCO_LANG, "en");
+        createDestinationDataFile(DESTINATION_NAME1, connectProperties);
+        connectProperties.setProperty(DestinationDataProvider.JCO_POOL_CAPACITY, "3");
+        connectProperties.setProperty(DestinationDataProvider.JCO_PEAK_LIMIT, "10");
+        createDestinationDataFile(DESTINATION_NAME2, connectProperties);
+
+    }
+
+    static void createDestinationDataFile(String destinationName, Properties connectProperties) {
+        File destCfg = new File(destinationName + ".jcoDestination");
+        try {
+            FileOutputStream fos = new FileOutputStream(destCfg, false);
+            connectProperties.store(fos, "for tests only !");
+            fos.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to create the destination files", e);
+        }
+    }
+
+    public static void step1Connect() throws JCoException {
+        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME1);
+        System.out.println("Attributes:");
+        System.out.println(destination.getAttributes());
+        System.out.println();
+    }
+
+    public static void step2ConnectUsingPool() throws JCoException {
+        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME2);
+        destination.ping();
+        System.out.println("Attributes:");
+        System.out.println(destination.getAttributes());
+        System.out.println();
+    }
+
+    public static void step3SimpleCall() throws JCoException {
+        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME2);
+        JCoFunction function = destination.getRepository().getFunction("STFC_CONNECTION");
+        if (function == null)
+            throw new RuntimeException("BAPI_COMPANYCODE_GETLIST not found in SAP.");
+        function.getImportParameterList().setValue("REQUTEXT", "Hello SAP");
+
+        try {
+            function.execute(destination);
+        } catch (AbapException e) {
+            System.out.println(e.toString());
+            return;
+        }
+
+        System.out.println("STFC_CONNECTION finished:");
+        System.out.println(" Echo: " + function.getExportParameterList().getString("ECHOTEXT"));
+        System.out.println(" Response: " + function.getExportParameterList().getString("RESPTEXT"));
+        System.out.println();
+    }
+
+    public static void step3WorkWithStructure() throws JCoException {
+        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME2);
+        JCoFunction function = destination.getRepository().getFunction("RFC_SYSTEM_INFO");
+        if (function == null)
+            throw new RuntimeException("BAPI_COMPANYCODE_GETLIST not found in SAP.");
+        try {
+            function.execute(destination);
+        } catch (AbapException e) {
+            System.out.println(e.toString());
+            return;
+        }
+
+        JCoStructure exportStructure = function.getExportParameterList().getStructure("RFCSI_EXPORT");
+        System.out.println("System info for " + destination.getAttributes().getSystemID() + ":\n");
+        for (int i = 0; i < exportStructure.getMetaData().getFieldCount(); i++) {
+            System.out.println(exportStructure.getMetaData().getName(i) + ":\t" + exportStructure.getString(i));
+        }
+        System.out.println();
+
+        //JCo still supports the JCoFields, but direct access via getXX is more efficient as field iterator
+        System.out.println("The same using field iterator: \nSystem info for " + destination.getAttributes().getSystemID() + ":\n");
+        for (JCoField field : exportStructure) {
+            System.out.println(field.getName() + ":\t" + field.getString());
+        }
+        System.out.println();
+    }
+
+    public static void step4WorkWithTable() throws JCoException {
+        JCoDestination destination = JCoDestinationManager.getDestination(DESTINATION_NAME2);
+        JCoFunction function = destination.getRepository().getFunction("BAPI_COMPANYCODE_GETLIST");
+        if (function == null)
+            throw new RuntimeException("BAPI_COMPANYCODE_GETLIST not found in SAP.");
+        try {
+            function.execute(destination);
+        } catch (AbapException e) {
+            System.out.println(e.toString());
+            return;
+        }
+
+        JCoStructure returnStructure = function.getExportParameterList().getStructure("RETURN");
+        if (!(returnStructure.getString("TYPE").equals("") || returnStructure.getString("TYPE").equals("S"))) {
+            throw new RuntimeException(returnStructure.getString("MESSAGE"));
+        }
+
+        JCoTable codes = function.getTableParameterList().getTable("COMPANYCODE_LIST");
+        for (int i = 0; i < codes.getNumRows(); i++) {
+            codes.setRow(i);
+            System.out.println(codes.getString("COMP_CODE") + '\t' + codes.getString("COMP_NAME"));
+        }
+        codes.firstRow();
+        for (int i = 0; i < codes.getNumRows(); i++, codes.nextRow()) {
+            function = destination.getRepository().getFunction("BAPI_COMPANYCODE_GETDETAIL");
+            if (function == null)
+                throw new RuntimeException("BAPI_COMPANYCODE_GETDETAIL not found in SAP.");
+            function.getImportParameterList().setValue("COMPANYCODEID", codes.getString("COMP_CODE"));
+            function.getExportParameterList().setActive("COMPANYCODE_ADDRESS", false);
+
+            try {
+                function.execute(destination);
+            } catch (AbapException e) {
+                System.out.println(e.toString());
+                return;
+            }
+            returnStructure = function.getExportParameterList().getStructure("RETURN");
+            if (!(returnStructure.getString("TYPE").equals("") ||
+                returnStructure.getString("TYPE").equals("S") ||
+                returnStructure.getString("TYPE").equals("W"))) {
+                throw new RuntimeException(returnStructure.getString("MESSAGE"));
+            }
+
+            JCoStructure detail = function.getExportParameterList().getStructure("COMPANYCODE_DETAIL");
+
+            System.out.println(detail.getString("COMP_CODE") + '\t' +
+                detail.getString("COUNTRY") + '\t' +
+                detail.getString("CITY"));
+        }
+    }
+
+    public static void main(String[] args) throws JCoException {
+        step1Connect();
+    }
+}
---
+++ src/main/java/SampleAbapConnectorServer.java
@@ -0,0 +1,116 @@
+import com.sap.conn.jco.JCoException;
+import com.sap.conn.jco.ext.ServerDataProvider;
+import com.sap.conn.jco.server.*;
+
+import java.io.*;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class SampleAbapConnectorServer implements JCoServerErrorListener, JCoServerExceptionListener, JCoServerStateChangedListener {
+
+  private static Logger logger = Logger.getLogger(SampleAbapConnectorServer.class);
+
+  /**
+   * The properties necessary to define the server and destination.
+   */
+  private Properties properties;
+
+  public SampleAbapConnectorServer() throws IOException {
+    InputStream propertiesInputStream =
+        SampleAbapConnectorServer.class.getClassLoader().getResourceAsStream("jco.properties");
+    properties = new Properties();
+    properties.load(propertiesInputStream);
+
+    new MyDestinationDataProvider(properties);
+    new MyServerDataProvider(properties);
+  }
+
+  /**
+   * Runnable to listen to the standard input stream to end the server.
+   */
+  private Runnable stdInListener = new Runnable() {
+
+    @Override
+    public void run() {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      String line = null;
+      try {
+        while((line = br.readLine()) != null) {
+          // Check if the server should be ended.
+          if(line.equalsIgnoreCase("end")) {
+            // Stop the server.
+            server.stop();
+          }
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  };
+
+  private JCoServer server;
+
+  public void serve() {
+    try {
+      server = JCoServerFactory.getServer(properties.getProperty(ServerDataProvider.JCO_PROGID));
+    } catch(JCoException e) {
+      throw new RuntimeException("Unable to create the server " + properties.getProperty(ServerDataProvider.JCO_PROGID) + ", because of " + e.getMessage(), e);
+    }
+
+    JCoServerFunctionHandler abapCallHandler = new AbapCallHandler();
+    DefaultServerHandlerFactory.FunctionHandlerFactory factory = new DefaultServerHandlerFactory.FunctionHandlerFactory();
+    factory.registerHandler(AbapCallHandler.FUNCTION_NAME, abapCallHandler);
+    server.setCallHandlerFactory(factory);
+
+    // Add listener for errors.
+    server.addServerErrorListener(this);
+    // Add listener for exceptions.
+    server.addServerExceptionListener(this);
+    // Add server state change listener.
+    server.addServerStateChangedListener(this);
+
+    // Add a stdIn listener.
+    new Thread(stdInListener).start();
+
+    // Start the server
+    server.start();
+//    logger.info("The program can be stopped typing 'END'");
+  }
+
+  @Override
+  public void serverExceptionOccurred(JCoServer jcoServer, String connectionId, JCoServerContextInfo arg2, Exception exception) {
+    logger.error("Exception occured on " + jcoServer.getProgramID() + " connection " + connectionId, exception);
+  }
+
+  @Override
+  public void serverErrorOccurred(JCoServer jcoServer, String connectionId, JCoServerContextInfo arg2, Error error) {
+    logger.error("Error occured on " + jcoServer.getProgramID() + " connection " + connectionId, error);
+  }
+
+  @Override
+  public void serverStateChangeOccurred(JCoServer server, JCoServerState oldState, JCoServerState newState) {
+    // Defined states are: STARTED, DEAD, ALIVE, STOPPED;
+    // see JCoServerState class for details.
+    // Details for connections managed by a server instance
+    // are available via JCoServerMonitor
+    logger.info("Server state changed from " + oldState.toString() + " to " + newState.toString() +
+        " on server with program id " + server.getProgramID());
+    if(newState.equals(JCoServerState.ALIVE)) {
+      logger.info("Server with program ID '"+server.getProgramID()+"' is running");
+    }
+    if(newState.equals(JCoServerState.STOPPED)) {
+      logger.info("Exit program");
+      System.exit(0);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+//    if(args.length == 0) {
+//      logger.error("You must specify a properties file!");
+//      System.out.println("You must specify a properties file!");
+//      return;
+//    }
+    new SampleAbapConnectorServer().serve();
+  }
+}

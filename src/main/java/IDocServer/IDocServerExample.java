8f3d32d2b782b1113fb8e0933f2b4875eebe010c
pupiwang <dreamjl@live.cn>
2018-12-20 15:05:12 +0800

    Extract classes

--- src/main/java/IDocServer/IDocServerExample.java
+++ src/main/java/IDocServer/IDocServerExample.java
@@ -1,12 +1,7 @@
 package IDocServer;
 
-import com.sap.conn.idoc.*;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-
-import com.sap.conn.jco.server.*;
-import com.sap.conn.idoc.jco.*;
+import com.sap.conn.idoc.jco.JCoIDoc;
+import com.sap.conn.idoc.jco.JCoIDocServer;
 
 public class IDocServerExample {
   public static void main(String[] a) {
@@ -23,76 +18,6 @@ public class IDocServerExample {
       server.start();
     } catch (Exception e) {
       e.printStackTrace();
-    }
-  }
-
-  static class MyIDocHandler implements JCoIDocHandler {
-    public void handleRequest(JCoServerContext serverCtx, IDocDocumentList idocList) {
-
-      FileOutputStream fos = null;
-      OutputStreamWriter osw = null;
-      try {
-        IDocXMLProcessor xmlProcessor =
-            JCoIDoc.getIDocFactory().getIDocXMLProcessor();
-        fos = new FileOutputStream(serverCtx.getTID() + "_idoc.xml");
-        osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
-        xmlProcessor.render(idocList, osw,
-            IDocXMLProcessor.RENDER_WITH_TABS_AND_CRLF);
-        osw.flush();
-      } catch (Throwable thr) {
-        thr.printStackTrace();
-      } finally {
-        try {
-          if (osw != null)
-            osw.close();
-          if (fos != null)
-            fos.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-  }
-
-  static class MyIDocHandlerFactory implements JCoIDocHandlerFactory {
-    private JCoIDocHandler handler = new MyIDocHandler();
-
-    public JCoIDocHandler getIDocHandler(JCoIDocServerContext serverCtx) {
-      return handler;
-    }
-  }
-
-  static class MyThrowableListener implements JCoServerErrorListener, JCoServerExceptionListener {
-
-    @Override
-    public void serverErrorOccurred(JCoServer jCoServer, String s, JCoServerContextInfo jCoServerContextInfo, Error error) {
-      System.out.println(">>> Error occured on " + jCoServer.getProgramID() + " connection " + jCoServerContextInfo.getConnectionID());
-      error.printStackTrace();
-    }
-
-    @Override
-    public void serverExceptionOccurred(JCoServer jCoServer, String s, JCoServerContextInfo jCoServerContextInfo, Exception error) {
-      System.out.println(">>> Error occured on " + jCoServer.getProgramID() + " connection " + jCoServerContextInfo.getConnectionID());
-      error.printStackTrace();
-    }
-  }
-
-  static class MyTidHandler implements JCoServerTIDHandler {
-    public boolean checkTID(JCoServerContext serverCtx, String tid) {
-      System.out.println("checkTID called for TID=" + tid);
-      return true;
-    }
-
-    public void confirmTID(JCoServerContext serverCtx, String tid) {
-      System.out.println("confirmTID called for TID=" + tid);
-    }
-
-    public void commit(JCoServerContext serverCtx, String tid) {
-      System.out.println("commit called for TID=" + tid);
-    }
-
-    public void rollback(JCoServerContext serverCtx, String tid) {
-      System.out.print("rollback called for TID=" + tid);
     }
   }
 }
---
+++ src/main/java/IDocServer/MyIDocHandlerFactory.java
@@ -0,0 +1,13 @@
+package IDocServer;
+
+import com.sap.conn.idoc.jco.JCoIDocHandler;
+import com.sap.conn.idoc.jco.JCoIDocHandlerFactory;
+import com.sap.conn.idoc.jco.JCoIDocServerContext;
+
+public class MyIDocHandlerFactory implements JCoIDocHandlerFactory {
+  private JCoIDocHandler handler = new MyIDocHandler();
+
+  public JCoIDocHandler getIDocHandler(JCoIDocServerContext serverCtx) {
+    return handler;
+  }
+}
